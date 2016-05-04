b7e0475c78caf7a6948af7e6accae9a61730704d
Luc Awater <lucawater@gmail.com>
2016-05-04 11:22:49 +0200

    Build header elements

--- content/themes/elements/content-post.php
+++ content/themes/elements/content-post.php
@@ -2,7 +2,8 @@
 // Content (variables)
 $title = get_the_title();
 $category = get_the_category( $post->ID );
-$category_link = get_category_link( $category[0]->term_id );
+( $category ) ? $category_link = get_category_link( $category[0]->term_id ) : $category_link = '';
+( $category ) ? $category_name = $category[0]->cat_name : $category_name = '';
 $content = wpautop( get_the_content() );
 $permalink = get_the_permalink();
 $date = get_the_date();
@@ -11,7 +12,7 @@ $thumb = get_the_post_thumbnail( $post->ID, 'medium' );
 
 <li>
   <div>
-    <a href="<?php echo $category_link; ?>" class="post-category"><?php echo $category[0]->cat_name; ?></a>
+    <a href="<?php echo $category_link; ?>" class="post-category"><?php echo $category_name; ?></a>
     <a class="post-title" href="<?php echo $permalink; ?>"><h3><?php echo $title; ?></h3></a>
   </div>
 
