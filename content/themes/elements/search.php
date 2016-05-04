c0c05470b93fd34792e3e68c6ae800fe334606c7
Luc Awater <lucawater@gmail.com>
2016-05-04 14:10:48 +0200

    Add 'no results' to search results page

--- content/themes/elements/search.php
+++ content/themes/elements/search.php
@@ -10,25 +10,25 @@ $searchQuery = preg_replace('/\s+/', '-', $searchQuery);
  * Get LEAGUES that match search criteria
  */
 $leagues = get_terms('league', array('hide_empty' => false));
-$results = array();
+$results_leagues = array();
 
 // Check if searchQuery matches (piece of) team slug and push to results array
 if( $leagues ):
   foreach( $leagues as $league ):
     if (strpos($league->slug, $searchQuery) !== false) {
-      array_push($results, $league);
+      array_push($results_leagues, $league);
     }
   endforeach;
 endif;
 
 // List results
-if( $results ):
+if( $results_leagues ):
 
   echo '<div class="results results-leagues">';
     echo '<h2>Leagues</h2>';
     echo '<ul>';
 
-      foreach( $results as $result ):
+      foreach( $results_leagues as $result ):
         // Check if search query against team slug
         $name = $result->name;
         // $name = preg_replace("/\p{L}*?".preg_quote($searchQuery)."\p{L}*/ui", "<b>$0</b>", $name);
@@ -46,25 +46,25 @@ endif;
  * Get TEAMS that match search criteria
  */
 $teams = get_terms('team', array('hide_empty' => false));
-$results = array();
+$results_teams = array();
 
 // Check if searchQuery matches (piece of) team slug and push to results array
 if( $teams ):
   foreach( $teams as $team ):
     if (strpos($team->slug, $searchQuery) !== false) {
-      array_push($results, $team);
+      array_push($results_teams, $team);
     }
   endforeach;
 endif;
 
 // List results
-if( $results ):
+if( $results_teams ):
 
   echo '<div class="results results-teams">';
     echo '<h2>Teams</h2>';
     echo '<ul>';
 
-      foreach( $results as $result ):
+      foreach( $results_teams as $result ):
         // Check if search query against team slug
         $name = $result->name;
         // $name = preg_replace("/\p{L}*?".preg_quote($searchQuery)."\p{L}*/ui", "<b>$0</b>", $name);
@@ -75,6 +75,18 @@ if( $results ):
 
     echo '</ul>';
   echo '</div>';
+
+endif;
+
+
+
+if( !$results_leagues && !$results_teams ):
+
+  echo
+  '<div class="no-results">
+    <p>Sorry, no matches found for <strong>' . $searchQuery . '</strong></p>
+    <p>Check the spelling or try different/fewer keywords and try again</p>
+  </div>';
 
 endif;
 
