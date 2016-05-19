60287462c9d30472107f8fa3ddeeaa1c75a1a5e1
Luc Awater <lucawater@gmail.com>
2016-05-19 17:13:14 +0200

    Tweak page header style

--- content/themes/elements/includes/functions-template/page-header-taxonomy.php
+++ content/themes/elements/includes/functions-template/page-header-taxonomy.php
@@ -14,12 +14,13 @@ $image = get_field( $term_taxonomy . '_imageFeatured', $term_acf );
 
 <section class="page-header page-header-taxonomy">
   <div class="section-body">
-    <h2><?php echo $term->name; ?></h2>
-
     <figure>
       <img src="<?php echo $image['sizes']['medium_large']; ?>" width="<?php echo $images['sizes']['image-width']; ?>" height="<?php echo $image['sizes']['image-height']; ?>" />
     </figure>
 
-    <div><?php echo wpautop($term->description); ?></div>
+    <div>
+      <h2><?php echo $term->name; ?></h2>
+      <?php echo wpautop($term->description); ?>
+    </div>
   </div>
 </section>
