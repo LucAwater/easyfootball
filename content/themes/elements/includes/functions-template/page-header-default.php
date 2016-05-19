163aa52b1a1bab0d7fe093e1b3e9e8d57c95a4c5
Luc Awater <lucawater@gmail.com>
2016-05-19 14:49:46 +0200

    Add default page header

---
+++ content/themes/elements/includes/functions-template/page-header-default.php
@@ -0,0 +1,5 @@
+<section class="page-header page-header-default">
+  <div class="section-body">
+    <h2>Page header</h2>
+  </div>
+</section>
--- content/themes/elements/includes/functions-template/page-header.php
+++ content/themes/elements/includes/functions-template/page-header.php
@@ -2,10 +2,10 @@
 function page_header() {
   if( is_front_page() ):
     include_once('page-header-home.php');
-  endif;
-
-  if( is_tax() ):
-    include('page-header-taxonomy.php');
+  elseif( is_tax() ):
+    include_once('page-header-taxonomy.php');
+  else:
+    include_once('page-header-default.php');
   endif;
 }
 ?>
