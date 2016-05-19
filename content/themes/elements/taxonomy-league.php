6c0cdd6267857c4200115a94c2a671b30103f620
Luc Awater <lucawater@gmail.com>
2016-05-19 10:42:57 +0200

    Tweak layout and style sidebar

--- content/themes/elements/taxonomy-league.php
+++ content/themes/elements/taxonomy-league.php
@@ -2,6 +2,7 @@
 get_header();
 
 $term =	$wp_query->queried_object;
+$term_acf = $term->taxonomy . '_' . $term->term_id;
 /**
  * woocommerce_before_main_content hook.
  *
@@ -14,7 +15,56 @@ do_action( 'woocommerce_before_main_content' );
  * League intro
  */
 get_template_part( 'taxonomy', 'header' );
+
+/**
+ * Sidebar
+ */
+$region = get_field( 'league_region', $term_acf );
+$region_id = $region->taxonomy . '_' . $region->term_id;
+$region_leagues = get_field( 'region_leagues', $region_id );
 ?>
+
+<?php if( $region_leagues ): ?>
+  <aside>
+    <div>
+      <h4 class="aside-subheader">Other English leagues</h4>
+
+      <ul>
+
+        <?php
+        foreach($region_leagues as $league):
+          if( $term->term_id != $league ){
+            $league_term = get_term_by('id', $league, 'league');
+
+            echo '<li><a href="' . get_term_link($league) . '">' . $league_term->name . '</a></li>';
+          }
+        endforeach;
+        ?>
+      </ul>
+    </div>
+
+    <div>
+      <h4 class="aside-subheader">Top Games</h4>
+
+      <ul>
+        <li>
+          <a>Aston Villa - Chelsea</a>
+          <small>21 July 2016 at 16:00</small>
+        </li>
+
+        <li>
+          <a>Manchester United - Barcelona</a>
+          <small>21 July 2016 at 16:00</small>
+        </li>
+
+        <li>
+          <a>Ajax - Real Madrid</a>
+          <small>21 July 2016 at 16:00</small>
+        </li>
+      </ul>
+    </div>
+  </aside>
+<?php endif; ?>
 
 <?php
 /*
