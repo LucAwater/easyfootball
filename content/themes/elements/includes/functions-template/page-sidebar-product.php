057d5c3b5b378108b95dfa80cf04f8399a78ad64
Luc Awater <lucawater@gmail.com>
2016-06-05 10:18:22 +0200

    Add fallback for productpage sidebar

--- content/themes/elements/includes/functions-template/page-sidebar-product.php
+++ content/themes/elements/includes/functions-template/page-sidebar-product.php
@@ -6,23 +6,31 @@
  */
 $match_location = get_field('match_location');
 
-if( $match_location ){
+if( $match_location ):
   // Get category term to retrieve custom fields of (sub)category
-  $category = get_category($match_location->term_id);
+  $category = $match_location;
   $category_tax = $category->taxonomy;
   $category_id = $category->term_id;
   $category_term = $category_tax . '_' . $category_id;
-}
 
-$map = get_field('seating_map', $category_term);
-$map_url = $map['sizes']['medium'];
-$map_width = $map['sizes']['medium-width'];
-$map_height = $map['sizes']['medium-height'];
-?>
-<aside>
-  <div>
-    <figure class="product-seating">
-      <img src="<?php echo $map_url; ?>" width="<?php echo $map_width; ?>" height="<?php echo $map_height; ?>" />
-    </figure>
-  </div>
-</aside>
+  $map = get_field('seating_map', $category_term);
+  $map_url = $map['sizes']['medium'];
+  $map_width = $map['sizes']['medium-width'];
+  $map_height = $map['sizes']['medium-height'];
+  ?>
+  <aside>
+    <div>
+      <figure class="product-seating">
+        <img src="<?php echo $map_url; ?>" width="<?php echo $map_width; ?>" height="<?php echo $map_height; ?>" />
+      </figure>
+    </div>
+  </aside>
+<?php else: ?>
+  <aside>
+    <div>
+      <figure class="product-seating">
+        <p>no product seating map available</p>
+      </figure>
+    </div>
+  </aside>
+<?php endif; ?>
