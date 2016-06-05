cae8da15e7188bd5ed336d0e4e64cf094e7f57f2
Luc Awater <lucawater@gmail.com>
2016-06-05 10:33:39 +0200

    Add countries to nav

--- content/themes/elements/includes/nav.php
+++ content/themes/elements/includes/nav.php
@@ -1,5 +1,6 @@
 <nav>
   <ul>
+    <!-- Teams -->
     <li>
       <a href="<?php echo home_url() . '/teams'; ?>">Lag</a>
       <ul>
@@ -11,7 +12,31 @@
         <li class="view-more"><a href="<?php echo home_url() . '/teams'; ?>">view more teams</a></li>
       </ul>
     </li>
+
+    <!-- Leagues -->
     <li><a href="<?php echo home_url() . '/leagues'; ?>">Ligor</a></li>
-    <li><p>Land</p></li>
+
+    <!-- Regions -->
+    <li>
+      <a href="<?php echo home_url() . '/regions'; ?>">Land</a>
+      
+      <?php
+      $regions = get_terms( 'region', array('hide_empty' => false,) );
+
+      if( $regions ):
+        echo '<ul>';
+
+          foreach( $regions as $region ){
+            $region_name = $region->name;
+            $region_link = get_term_link($region->term_id, 'region');
+
+            echo '<li><a href="' . $region_link . '">' . $region_name . '</a>';
+          }
+
+        echo '</ul>';
+      endif;
+      ?>
+    </li>
+
   </ul>
 </nav>
