8832389ef9bce61dc1aaf141d6810ca5a8a2b8ed
Luc Awater <lucawater@gmail.com>
2016-06-10 15:13:19 +0200

    Remove woocommerce's select replacement

--- content/themes/elements/functions.php
+++ content/themes/elements/functions.php
@@ -63,6 +63,19 @@ function redirect_to_checkout() {
   return $checkout_url;
 }
 
+// Removing Woocommerce's standard select replacement
+add_action( 'wp_enqueue_scripts', 'mgt_dequeue_stylesandscripts', 100 );
+
+function mgt_dequeue_stylesandscripts() {
+  if ( class_exists( 'woocommerce' ) ) {
+    wp_dequeue_style( 'select2' );
+    wp_deregister_style( 'select2' );
+
+    wp_dequeue_script( 'select2');
+    wp_deregister_script('select2');
+  }
+}
+
 // Customize excerpt
 function wpdocs_excerpt_more( $more ) {
   return '...';
