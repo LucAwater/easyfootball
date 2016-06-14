5b8445880ad5ff61b2b46d23467989781a8d3938
Luc Awater <lucawater@gmail.com>
2016-06-14 13:13:16 +0200

    Add locations to search results

--- content/themes/elements/search.php
+++ content/themes/elements/search.php
@@ -6,24 +6,24 @@ $searchQuery = get_search_query();
 $searchQuery = strtolower($searchQuery);
 $searchQuery = preg_replace('/\s+/', '-', $searchQuery);
 
-/*
- * Get LEAGUES that match search criteria
- */
-$leagues = get_terms('league', array('hide_empty' => false));
-$results_leagues = array();
-
-// Check if searchQuery matches (piece of) team slug and push to results array
-if( $leagues ):
-  foreach( $leagues as $league ):
-    if (strpos($league->slug, $searchQuery) !== false) {
-      array_push($results_leagues, $league);
-    }
-  endforeach;
-endif;
-
 page_content_start();
 
-  // List results
+  /*
+   * Get LEAGUES that match search criteria
+   */
+  $leagues = get_terms('league', array('hide_empty' => false));
+  $results_leagues = array();
+
+  // Check if searchQuery matches (piece of) team slug and push to results array
+  if( $leagues ):
+    foreach( $leagues as $league ):
+      if (strpos($league->slug, $searchQuery) !== false) {
+        array_push($results_leagues, $league);
+      }
+    endforeach;
+  endif;
+
+  // List league results
   if( $results_leagues ):
 
     echo '<div class="results results-leagues">';
@@ -33,7 +33,8 @@ page_content_start();
         foreach( $results_leagues as $result ):
           // Check if search query against team slug
           $name = $result->name;
-          // $name = preg_replace("/\p{L}*?".preg_quote($searchQuery)."\p{L}*/ui", "<b>$0</b>", $name);
+          $name = str_replace($searchQuery, "<strong>" . $searchQuery . "</strong>", $name);
+          $bodytag = str_replace("%body%", "black", "<body text='%body%'>");
           $url = get_category_link( $result->term_id );
 
           echo '<li><a href="' . $url . '">' . $name . '</a></li>';
@@ -49,6 +50,7 @@ page_content_start();
    */
   $teams = get_terms('team', array('hide_empty' => false));
   $results_teams = array();
+  $results_locations = array();
 
   // Check if searchQuery matches (piece of) team slug and push to results array
   if( $teams ):
@@ -59,7 +61,7 @@ page_content_start();
     endforeach;
   endif;
 
-  // List results
+  // List team results
   if( $results_teams ):
 
     echo '<div class="results results-teams">';
@@ -80,9 +82,66 @@ page_content_start();
 
   endif;
 
+  /*
+   * Get LOCATIONS that match search criteria
+   */
+  // Check if searchQuery matches (piece of) team "arena_location_city" field and push to results array
+  if( $teams ):
+    foreach( $teams as $team ):
+      $team_acf = $team->taxonomy . '_' . $team->term_id;
+      $team_location = get_field('arena_location_city', $team_acf);
 
+      if(stripos($team_location, $searchQuery) !== false) {
+        array_push($results_locations, $team->name);
+      }
+    endforeach;
+  endif;
 
-  if( !$results_leagues && !$results_teams ):
+  if( $results_locations ):
+    // List location results
+    $matches_args = array(
+      'post_type'   => 'product',
+      'posts_per_page' => 10,
+      'order'       => 'asc',
+      'meta_query'  => array(
+        array(
+          'key'     => 'match_location',
+          'value'   => $results_locations,
+          'compare' => 'IN',
+        )
+      )
+    );
+    $matches = new WP_Query($matches_args);
+
+    if( $matches->have_posts() ):
+      ?>
+
+      <section class="matches">
+        <h3>Upcoming matches in <?php echo $searchQuery; ?></h3>
+        <ul class="list list-col">
+          <li class="list-col-head">
+            <p class="list-item-20">Date</p>
+            <p class="list-item-40">Match</p>
+            <p class="list-item-20">Prices</p>
+            <p class="list-item-20"></p>
+          </li>
+
+          <?php
+          while( $matches->have_posts() ): $matches->the_post();
+            wc_get_template_part( 'woocommerce/content', 'product' );
+          endwhile;
+
+        echo '</ul>';
+      echo '</section>';
+
+    endif;
+
+    wp_reset_query();
+
+  endif;
+
+  // No results
+  if( !$results_leagues && !$results_teams && !$results_locations ):
 
     echo
     '<div class="no-results">
