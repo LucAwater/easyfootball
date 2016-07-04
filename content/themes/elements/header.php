6d11fcf15111f3b0b60146a4854fbf3fccafc637
Luc Awater <lucawater@gmail.com>
2016-07-04 11:08:19 +0200

    Tweak icon in header button

--- content/themes/elements/header.php
+++ content/themes/elements/header.php
@@ -63,7 +63,7 @@
 
         <?php include_once( 'includes/nav.php' ); ?>
 
-        <a class="button button-sec" href="<?php echo home_url(); ?>/kundtjanst"><i class="material-icons">&#xE311;</i> Kundtjänst</a>
+        <a class="button button-sec" href="<?php echo home_url(); ?>/kundtjanst"><i class="material-icons">&#xE0CA;</i> Kundtjänst</a>
       </div>
     </div>
 
