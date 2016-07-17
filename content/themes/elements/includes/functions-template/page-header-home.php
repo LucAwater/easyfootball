68a531a6bb469a48469cb7f77db0907b02b5fa70
Luc Awater <lucawater@gmail.com>
2016-07-17 14:57:37 +0200

    Update home header subtitle

--- content/themes/elements/includes/functions-template/page-header-home.php
+++ content/themes/elements/includes/functions-template/page-header-home.php
@@ -9,7 +9,7 @@ $image_url = $image["sizes"]["large"];
   id="intro-home"
   style="background-image: url(<?php echo $image_url; ?>)">
   <h1>Easyfootball.se</h1>
-  <h3>Köp fotbollsbiljetter till alla världens matcher</h3>
+  <h3><?php _e('Köp fotbollsbiljetter till världens alla matcher'); ?></h3>
 
   <form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
     <input type="search" pattern=".{3,}" title="3 characters minimum" required placeholder="<?php echo esc_attr_x( 'Sök lag, ligor, platser...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" />
