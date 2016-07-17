6aa6cbf5c47f8eaf11a5a4b3aea84cc2a15e73c5
Luc Awater <lucawater@gmail.com>
2016-07-17 15:42:57 +0200

    Translate billing fields

--- content/plugins/woocommerce/templates/checkout/form-billing.php
+++ content/plugins/woocommerce/templates/checkout/form-billing.php
@@ -30,7 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<?php else : ?>
 
-		<h3><?php _e( 'Billing Details', 'woocommerce' ); ?></h3>
+		<h3><?php _e( 'Dina uppgifter', 'woocommerce' ); ?></h3>
 
 	<?php endif; ?>
 
--- content/themes/elements/includes/functions-woocommerce/checkout.php
+++ content/themes/elements/includes/functions-woocommerce/checkout.php
@@ -4,10 +4,19 @@ add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
 
 // Our hooked in function - $fields is passed via the filter!
 function custom_override_checkout_fields( $fields ) {
+  $fields['billing']['billing_first_name']['label'] = 'Förnamn';
+  $fields['billing']['billing_last_name']['label'] = 'Efternamn';
+  $fields['billing']['billing_address_1']['label'] = 'Adress';
+  $fields['billing']['billing_city']['label'] = 'Postort';
+  $fields['billing']['billing_postcode']['label'] = 'Postnummer';
+  $fields['billing']['billing_country']['label'] = 'Land';
+  $fields['billing']['billing_state']['label'] = 'State';
+  $fields['billing']['billing_email']['label'] = 'E-post';
+  $fields['billing']['billing_phone']['label'] = 'Mobilnummer';
+  
   unset($fields['billing']['billing_address_2']);
   unset($fields['billing']['billing_company']);
   unset($fields['order']['order_comments']);
-
 
   return $fields;
 }
@@ -17,9 +26,9 @@ function custom_override_checkout_fields( $fields ) {
  *
  * Reference: https://github.com/woothemes/woocommerce/blob/master/templates/checkout/form-checkout.php
  */
-add_action( 'woocommerce_before_checkout_form', 'skyverge_add_checkout_notice', 11 );
-
-function skyverge_add_checkout_notice() {
-  wc_print_notice( __( 'Tickets will be sent to the venue', 'woocommerce' ), 'notice' );
-}
+// add_action( 'woocommerce_before_checkout_form', 'skyverge_add_checkout_notice', 11 );
+//
+// function skyverge_add_checkout_notice() {
+//   wc_print_notice( __( 'Tickets will be sent to the venue', 'woocommerce' ), 'notice' );
+// }
 ?>
