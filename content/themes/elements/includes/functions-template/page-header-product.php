dbc7890a4830cb8beb04829ec3a23053800c7467
Luc Awater <lucawater@gmail.com>
2016-07-17 15:50:00 +0200

    Add fallback for timestamp

--- content/themes/elements/includes/functions-template/page-header-product.php
+++ content/themes/elements/includes/functions-template/page-header-product.php
@@ -21,8 +21,9 @@ if( $bg_url ){
     <?php
     // Get match data from custom fields of product
     $match_date_raw = get_field('match_date', false, false);
-    $match_date_raw = DateTime::createFromFormat('Y-m-j', $match_date_raw);
-    $match_date = dateFormat($match_date_raw, 'sv', '%A, %e %B %G')[0];
+    if($match_date_raw)
+      $match_date_raw = DateTime::createFromFormat('Y-m-j', $match_date_raw);
+      $match_date = dateFormat($match_date_raw, 'sv', '%A, %e %B %G')[0];
 
     $match_time = get_field('match_time');
     $match_location = get_field('match_location');
