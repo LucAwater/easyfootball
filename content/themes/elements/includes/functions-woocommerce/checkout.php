9602d6b15ecd6df2b72b04352fa78edf447b3ffc
Luc Awater <lucawater@gmail.com>
2016-07-27 13:45:54 +0200

    Fix billing field labels
    City and Postcode field labels were not edited using woocommerce_checkout_fields, now using gettext filter.

--- content/themes/elements/includes/functions-woocommerce/checkout.php
+++ content/themes/elements/includes/functions-woocommerce/checkout.php
@@ -7,19 +7,37 @@ function custom_override_checkout_fields( $fields ) {
   $fields['billing']['billing_first_name']['label'] = 'Förnamn';
   $fields['billing']['billing_last_name']['label'] = 'Efternamn';
   $fields['billing']['billing_address_1']['label'] = 'Adress';
+  $fields['billing']['billing_address_1']['placeholder'] = '';
   $fields['billing']['billing_city']['label'] = 'Postort';
   $fields['billing']['billing_postcode']['label'] = 'Postnummer';
   $fields['billing']['billing_country']['label'] = 'Land';
   $fields['billing']['billing_state']['label'] = 'State';
   $fields['billing']['billing_email']['label'] = 'E-post';
   $fields['billing']['billing_phone']['label'] = 'Mobilnummer';
-  
+
   unset($fields['billing']['billing_address_2']);
   unset($fields['billing']['billing_company']);
   unset($fields['order']['order_comments']);
 
   return $fields;
 }
+
+/**
+ * Extra function to replace city and postcode fields
+ * somehow, they get overwritten using just the woocommerce_checkout_fields filter
+ */
+function checkout_fields_override_extra( $translated_text, $text, $domain ) {
+  switch ( $translated_text ) {
+    case 'Postcode / ZIP' :
+      $translated_text = __( 'Postnummer', 'woocommerce' );
+      break;
+    case 'Town / City' :
+      $translated_text = __( 'Postort', 'woocommerce' );
+      break;
+  }
+  return $translated_text;
+}
+add_filter( 'gettext', 'checkout_fields_override_extra', 20, 3 );
 
 /**
  * Add an info notice instead. Let's add it after other notices with priority = 11
