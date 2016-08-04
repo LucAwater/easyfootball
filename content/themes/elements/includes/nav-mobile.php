f2f8f46334ff0616638e73df33f2a77c062cd2f0
Luc Awater <lucawater@gmail.com>
2016-08-04 12:00:42 +0200

    Tweak styling for mobile menu

--- content/themes/elements/includes/nav-mobile.php
+++ content/themes/elements/includes/nav-mobile.php
@@ -1,6 +1,6 @@
 <nav id="navMobile">
   <div>
-    <a id="navMobile-close"><?php _e('Stäng'); ?> <i class="material-icons">&#xE5CD;</i></a>
+    <a id="navMobile-close" class="button button-sec"><?php _e('Stäng'); ?> <i class="material-icons">&#xE5CD;</i></a>
   </div>
 
   <div id="navMobile-menu">
